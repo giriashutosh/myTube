19b78a65cbfb6e542d3802724c5553d33f0f0fa1
Ashutosh Giri <ashutoshgiri249@gmail.com>
2023-08-19 14:38:03 +0530

    add header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,24 +1,24 @@
 import React from 'react'
 import Hamburger from '../Assets/Images/Hamburger_icon.png'
 import Hippo from '../Assets/Images/hippo.png'
-import UserImg from '../Assets.Images/user.png'
+import UserImg from '../Assets/Images/user.png'
 
 const Header = () => {
     return (
-        <div className='grid grid-flow-col'>
+        <div className='grid grid-flow-col flex items-center p-2 shadow-lg'>
             {/* for hamberger and Logo */}
-            <div className='grid-col-1 bg-slate-100'>
-                <img className="w-10" src={Hamburger} alt="" />
-                <img className="w-10" src={Hippo} alt="" />
+            <div className='flex gap-4 grid-col-1  p-2'>
+                <img className="w-10 " src={Hamburger} alt="" />
+                <img className="w-20" src={Hippo} alt="" />
             </div>
             {/* for search  */}
-            <div className='grid-col-10 bg-slate-200 flex'>
-                <input className='w-50 border border-2'></input>
-                <div>Search</div>
+            <div className='grid-col-10  flex'>
+                <input className='w-[29rem] pl-2 border-2 rounded-l-full h-10 outline-none start-2'></input>
+                <button className='bg-gray-100 flex items-center p-2 rounded-r-full'>🔍</button>
             </div>
             {/* for user account  */}
-            <div className='grid-col-10 bg-slate-200 flex'>
-                <img src={UserImg} alt=""/>
+            <div className='grid-col-10  flex flex-row-reverse '>
+                <img className="w-10 py-2 pr-2 mr-2" src={UserImg} alt=""/>
             </div>
         </div>
     )
